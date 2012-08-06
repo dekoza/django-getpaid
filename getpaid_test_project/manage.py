afad9f96eff64ca197f6cbedcaca875b02d0a8d7
Dominik Kozaczko <dominik@bleys>
2012-08-06 13:39:26 +0200

    merge upstream; rename README

--- getpaid_test_project/manage.py
+++ getpaid_test_project/manage.py
@@ -1,8 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 import os
 import sys
 
 if __name__ == "__main__":
+    # Oh those hackish hackers!
+#    print os.path.abspath(os.path.dirname(__file__)+'/../')
+    sys.path.append(os.path.abspath(os.path.dirname(__file__)+'/../'))
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "getpaid_test_project.settings")
 
     from django.core.management import execute_from_command_line
